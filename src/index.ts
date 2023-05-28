e35b1bbbcebdfc483422ab2d6a3671de2af32b99
Sebastian Palmqvist <sebastian.palmqvist@appliedtechnology.se>
2023-05-28 15:20:04 +0200

    feat: implemented wordCounter and letterCounter

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
 import "./styles/index.css";
+const btn = document.querySelector('#count')
+const resEl: HTMLElement = document.querySelector('#results')
+const words: HTMLInputElement = document.querySelector('#textInput')
 
+const countWords = () => words.value.split(' ').length
+
+const countLetters = () => words.value.split(' ').join('').split('').length
+
+const appendToDom = (element: HTMLElement, targetEl: HTMLElement) => targetEl.append(element)
+
+const createResEl = (myFnc: Function) => {
+  const resultEl = document.createElement('p')
+  resultEl.textContent = myFnc().toString()
+  return resultEl
+}
+
+btn.addEventListener('click', () => { 
+  appendToDom(createResEl(countWords), resEl)
+  appendToDom(createResEl(countLetters), resEl)
+})
